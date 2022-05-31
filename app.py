b9f8c8a6991ff9c3d2123434b65c9b0a110cec46
James Fischer <jfischer@collectivehivemind.com>
2022-05-30 23:10:44 -0400

    add logging

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from datetime import datetime
+import logging
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
 
-    print({'timestamp': str(datetime.now()), 'message': 'A thing happened'})
+    logging.info(str({'timestamp': str(datetime.now()), 'message': 'A thing happened'}))
 
     return {
         "Temp": 50,
